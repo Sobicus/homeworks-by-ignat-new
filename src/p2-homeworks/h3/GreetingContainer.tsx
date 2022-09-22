37fb70d8d6bd45020197cc0b32e54eebdc069952
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-09-22 22:16:14 +0200

    'task3'

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // need to fix any
+    addUserCallback:  any // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,11 +13,11 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+        setName(e.name) // need to fix
     }
     const addUser = () => {
         alert(`Hello  !`) // need to fix
