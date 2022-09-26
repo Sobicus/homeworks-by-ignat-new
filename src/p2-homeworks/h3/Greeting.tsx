8e6d536cc09849b7b21a8ee94e4a2dce801e7efc
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-09-26 18:46:30 +0200

    HW3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -3,8 +3,8 @@ import s from './Greeting.module.css'
 
 type GreetingPropsType = {
     name: string // need to fix any
-    setNameCallback: (e: React.ChangeEvent<HTMLInputElement>)=>void // need to fix any
-    addUser: ()=>void // need to fix any
+    setNameCallback: (e: React.ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
     error: string // need to fix any
     totalUsers: number // need to fix any
 }
@@ -13,13 +13,13 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = (name.length === 0 ? s.error : '') // need to fix with (?:)
 
     return (
         <div>
-            <input value={name} onChange={(e)=>setNameCallback(e)} className={inputClass}/>
+            <input value={name} onChange={(e) => setNameCallback(e)} className={inputClass}/>
             <span>{error}</span>
-            <button onClick={addUser}>add</button>
+            <button onClick={addUser} disabled={name.length === 0}>add</button>
             <span>{totalUsers}</span>
         </div>
     )
